2b3e5ab5d686a511ee392603d0e56dd85969492d
AlissonGS <alissonofficial12@gmail.com>
2022-07-14 15:00:17 -0300

    Serialização de todas as classes

--- livraria/urls.py
+++ livraria/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-from core.views import CategoriaViewSet
+from core import views
 
 router = DefaultRouter()
-router.register(r'categorias', CategoriaViewSet)
+router.register(r'categorias', views.CategoriaViewSet)
+router.register(r'editoras', views.EditoraViewSet)
+router.register(r'autores', views.AutorViewSet)
+router.register(r'livros', views.LivroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
